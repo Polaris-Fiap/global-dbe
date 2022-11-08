4c39216571a63105d38af535970bf41e233a4b32
Claudio Jin <natsu165@hotmail.com>
2022-11-08 00:34:49 -0300

    first commit

---
+++ src/main/java/br/com/fiap/globalSolutionPolaris/GlobalSolutionPolarisApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.globalSolutionPolaris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalSolutionPolarisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalSolutionPolarisApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/fiap/globalSolutionPolaris/model/Estado.java
@@ -0,0 +1,8 @@
+package br.com.fiap.globalSolutionPolaris.model;
+
+@Entity
+public class Estado {
+    private Long codEstado;
+    private String nomeEstado;
+    private String siglaEstado;
+}
