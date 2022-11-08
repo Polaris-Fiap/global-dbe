d9333d806e26faf0f93f080ddfa711b5dd229031
Claudio Jin <natsu165@hotmail.com>
2022-11-08 01:24:35 -0300

    criar endereco controller

---
+++ src/main/java/br/com/fiap/globalSolutionPolaris/controller/EnderecoController.java
@@ -0,0 +1,56 @@
+package br.com.fiap.globalSolutionPolaris.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.fiap.globalSolutionPolaris.model.Endereco;
+import br.com.fiap.globalSolutionPolaris.service.EnderecoService;
+
+@RestController
+@RequestMapping("api/endereco")
+public class EnderecoController{
+
+    @Autowired
+    private EnderecoService enderecoService;
+
+    @PostMapping
+    public ResponseEntity<Endereco> create(@RequestBody @Valid Endereco endereco) {
+        enderecoService.save(endereco);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(endereco);
+    }
+
+    @GetMapping
+    public List<Endereco> index() {
+        return enderecoService.listAll();
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Endereco> updateEnderecoById(@PathVariable Long cep, @RequestBody @Valid Endereco novoEndereco){
+        var optional = enderecoService.getById(cep);
+
+        if(optional.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+        var endereco = optional.get();
+        BeanUtils.copyProperties(novoEndereco, endereco);
+        endereco.setNumeroCep(cep);
+
+        enderecoService.save(endereco);
+        return ResponseEntity.ok(endereco);
+    }
+}
